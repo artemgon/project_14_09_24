4ab85cfc8b0a0ea463c874af655c2241f1e1e7bd
artemgon <artemhoncharuk123@gmail.com>
2024-12-01 14:48:34 +0200

    PE is completed.

--- classes.h
+++ classes.h
@@ -1,1 +1,56 @@
 #include "libs.h"
+
+class Point
+{
+private:
+	float x, y, z;
+public:
+	Point()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	Point(float u_x, float u_y, float u_z)
+	{
+		x = u_x;
+		y = u_y;
+		z = u_z;
+	}
+    void printFunc() const
+    {
+		cout << endl;
+		cout << "Point info: " << endl;
+		cout << "(" << x << ", " << y << ", " << z << ")" << endl;
+    }
+	void setValues(float u_x_1, float u_y_1, float u_z_1)
+	{
+		x = u_x_1;
+		y = u_y_1;
+		z = u_z_1;
+	}
+	void setX(const float& u_x_2)
+	{
+		x = u_x_2;
+	}
+	void setY(const float& u_y_2)
+	{
+		y = u_y_2;
+	}
+	void setZ(const float& u_z_2)
+	{
+		z = u_z_2;
+	}
+	float getX() const
+	{
+		return x;
+	}
+	float getY() const
+	{ 
+		return y;
+	}
+	float getZ() const 
+	{
+		return z;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+	Point p1(5, 6, 7), p2(15, 26, 11), p3;
+	p3.setValues(1, 7, 5);
+	p1.printFunc();
+	p2.printFunc();
+	p3.printFunc();
+	Point p4;
+	p4.setX(20);
+	p4.setY(17);
+	p4.setZ(18);
+	cout << endl;
+	cout << "Info about p4: " << endl;
+	cout << "x: " << p4.getX() << endl;
+	cout << "y: " << p4.getY() << endl;
+	cout << "z: " << p4.getZ() << endl;
 	return 0;
 }
