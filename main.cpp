b85cf07b52adcc4d7843aa04be214037feafeff4
artemgon <artemhoncharuk123@gmail.com>
2024-12-01 14:30:19 +0200

    Task 1 is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+    Student student(
+        "Kovalenko Ivan Vasylovych", "2000-05-15", "+123456789",
+        "Kyiv", "Ukraine",
+        "National University", "Kyiv", "Ukraine", 15.5);
+    student.printFunc();
+    cout << endl;
+    cout << "Get full name: " << student.getSNP() << endl;
+    student.setCity("Lviv");
+    student.setGNumber(202);
+    cout << endl;
+    cout << "After modifications: " << endl;
+    student.printFunc();
 	return 0;
 }
